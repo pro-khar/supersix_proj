f037f94300cdc6f0caf249b2f86ff5ddd8392748
pro-khar <prakharvermamanu.veed@gmail.com>
2024-05-27 06:08:37 +0530

    progress bar complete

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import { Progress } from "@/components/ui/progress";
 
 import { ScrollArea } from "./components/ui/scroll-area";
 import { Label } from "./components/ui/label";
@@ -33,17 +34,35 @@ function App() {
   const [data, setdata] = useState([]);
   const [fileName, setFileName] = useState("");
   const [rowsPerPageInput, setRowsPerPageInput] = useState(rowsPerPage);
+  const [progress, setprogress] = useState(10);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (e) => {
-    console.log("Done!");
     const file = e.target.files[0];
     setFileName(file.name);
-    Papa.parse(file, {
-      header: true,
-      complete: (results) => {
-        setdata(results.data);
-      },
-    });
+    setIsLoading(true);
+    setprogress(0);
+
+    const totalDuration = 1000;
+    const progressInterval = 100;
+    let elapsedTime = 0;
+
+    const interval = setInterval(() => {
+      elapsedTime += progressInterval;
+      const progressValue = (elapsedTime / totalDuration) * 100;
+      setprogress(progressValue);
+    }, progressInterval);
+
+    setTimeout(() => {
+      Papa.parse(file, {
+        header: true,
+        complete: (results) => {
+          setdata(results.data);
+          setIsLoading(false);
+          clearInterval(interval);
+        },
+      });
+    }, totalDuration);
   };
   useEffect(() => {
     setRowsPerPageInput(rowsPerPageInput);
@@ -65,20 +84,34 @@ function App() {
           <h1 className="scroll-m-20 border-b pb-2 text-2xl md:text-3xl xl:text-3xl 2xl:text-3xl font-semibold tracking-tight first:mt-0">
             {data.length ? fileName : "Upload a CSV file to continue"}
           </h1>
-          <div className="flex w-[70%] md:w-1/2 xl:w-1/2 2xl:w-1/2 gap-8">
+          <div className="flex w-[71%] gap-8 justify-center">
             <Input
               type="file"
               accept=".csv"
-              className=""
+              className="w-[500px] py-[5px]"
               onChange={handleSubmit}
             />
             {data.length ? (
-              <div className="flex gap- w-full ">
-                <Label className="w-full pt-2">Rows per page:</Label>
-                <Input type="number" value={rowsPerPageInput} onChange={(e)=>setRowsPerPageInput(Number(e.target.value))}/>
+              <div className="flex">
+                <Label className="w-[80px] pt-1">Rows per page:</Label>
+                <Input
+                  className="w-[80px]"
+                  type="number"
+                  value={rowsPerPageInput}
+                  onChange={(e) => setRowsPerPageInput(Number(e.target.value))}
+                />
               </div>
             ) : null}
+            {data.length ? (
+              <h1 className="w-full border rounded-md text-center pt-1">
+                Showing {startindex + 1} to {endindex} of {data.length - 1}{" "}
+                entries
+              </h1>
+            ) : null}
           </div>
+          {isLoading ? (
+            <Progress value={progress} className="w-[500px]" />
+          ) : null}
 
           {data.length ? (
             <ScrollArea
